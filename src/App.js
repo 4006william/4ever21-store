1989786a59b5d66379c69ecdb694b316017c00f7
wkiyola <wkiyola@yahoo.com>
2019-12-31 21:10:04 -0600

    even another commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css'
 import Router from './Router'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <Router />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   );
 }
