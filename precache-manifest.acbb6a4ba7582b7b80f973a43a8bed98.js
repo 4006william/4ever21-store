63af43cc36369fcfe6307e833331cb60a9abe9c1
wkiyola <wkiyola@yahoo.com>
2019-12-31 21:06:28 -0600

    Updates

---
+++ precache-manifest.acbb6a4ba7582b7b80f973a43a8bed98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d77604557471ecd706c822363773453",
+    "url": "/4ever21-store/index.html"
+  },
+  {
+    "revision": "47439b8043683039d496",
+    "url": "/4ever21-store/static/css/main.8e532f42.chunk.css"
+  },
+  {
+    "revision": "1f2461382b7a90a82679",
+    "url": "/4ever21-store/static/js/2.8a653a7f.chunk.js"
+  },
+  {
+    "revision": "63e1eed04f33c4a035dc50e6d9636111",
+    "url": "/4ever21-store/static/js/2.8a653a7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47439b8043683039d496",
+    "url": "/4ever21-store/static/js/main.03607601.chunk.js"
+  },
+  {
+    "revision": "c2b3c768e34c282ab6a4",
+    "url": "/4ever21-store/static/js/runtime-main.fa8f7bec.js"
+  },
+  {
+    "revision": "24de8ff2d487b5ffc48879bc7f06bf42",
+    "url": "/4ever21-store/static/media/logo.24de8ff2.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.060642765bae51bc9923baa879ff4d80.js"
+  "/4ever21-store/precache-manifest.acbb6a4ba7582b7b80f973a43a8bed98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/4ever21-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
